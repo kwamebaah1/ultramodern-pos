a1ebcfe169f85c49865a0448012917c4e133005c
kwamebaah1 <baahhprince79@gmail.com>
2025-04-30 00:25:55 +0000

    Supabase setup

---
+++ src/lib/supabase/server.js
@@ -0,0 +1,10 @@
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
+import { cookies } from 'next/headers';
+
+export const createClient = () => {
+  const cookieStore = cookies();
+  
+  return createServerComponentClient({
+    cookies: () => cookieStore
+  });
+};
