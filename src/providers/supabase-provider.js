146c57014e5d6d01abdd0b2f1b95162ba05d7d57
kwamebaah1 <baahhprince79@gmail.com>
2025-04-30 00:30:55 +0000

    Supabase setup

---
+++ src/providers/supabase-provider.js
@@ -0,0 +1,24 @@
+'use client';
+
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
+
+export default function SupabaseProvider({ children, session }) {
+  const [supabase] = useState(() => createClientComponentClient());
+  const router = useRouter();
+
+  useEffect(() => {
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((event, session) => {
+      if (event === 'SIGNED_IN') router.refresh();
+    });
+
+    return () => {
+      subscription?.unsubscribe();
+    };
+  }, [router, supabase]);
+
+  return children;
+}
