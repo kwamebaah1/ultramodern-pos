748f850b802a1053135c371da1d634c783e8fa8b
kwamebaah1 <baahhprince79@gmail.com>
2025-05-15 02:27:48 +0000

    Login

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useState } from 'react';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import toast from 'react-hot-toast';
 import { supabase } from '@/lib/supabase/client';
 import Head from 'next/head';
