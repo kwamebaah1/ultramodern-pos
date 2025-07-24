23a4a8fafa06913db87ff1a786d6ac8a75f999c7
kwamebaah1 <baahhprince79@gmail.com>
2025-07-24 23:57:49 +0000

    Currencies

---
+++ src/components/currencies/Currency.js
@@ -0,0 +1,9 @@
+export const CURRENCIES = [
+  { code: 'USD', name: 'US Dollar', symbol: '$' },
+  { code: 'EUR', name: 'Euro', symbol: '€' },
+  { code: 'GBP', name: 'British Pound', symbol: '£' },
+  { code: 'NGN', name: 'Nigerian Naira', symbol: '₦' },
+  { code: 'KES', name: 'Kenyan Shilling', symbol: 'KSh' },
+  { code: 'GHS', name: 'Ghanaian Cedi', symbol: 'GH₵' },
+  { code: 'ZAR', name: 'South African Rand', symbol: 'R' },
+];
