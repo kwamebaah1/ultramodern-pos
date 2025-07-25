a1d9e96b67c985bdcc3634a4e0f7c27a76aebcd9
kwamebaah1 <baahhprince79@gmail.com>
2025-07-25 17:36:39 +0000

    Login Edit

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -187,7 +187,7 @@ export default function Login() {
               </button>
             </form>
             
-            <div className="mt-6">
+            {/*<div className="mt-6">
               <div className="relative">
                 <div className="absolute inset-0 flex items-center">
                   <div className="w-full border-t border-gray-600"></div>
@@ -226,7 +226,7 @@ export default function Login() {
                   </svg>
                 </button>
               </div>
-            </div>
+            </div>*/}
             
             <div className="mt-6 text-center text-sm text-gray-400">
               Don't have an account?{' '}
