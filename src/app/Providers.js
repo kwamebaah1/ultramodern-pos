5152c65d35c125d0bf19356d453979a76ac18ee4
kwamebaah1 <baahhprince79@gmail.com>
2025-07-26 11:03:18 +0000

    Top bar

--- src/app/Providers.js
+++ src/app/Providers.js
@@ -1,7 +1,8 @@
-"use client"
+"use client";
 
 import SupabaseProvider from "@/providers/supabase-provider";
 import Sidebar from '@/components/Sidebar';
+import Topbar from '@/components/Topbar';
 import { ThemeProvider } from '@/components/ThemeProvider';
 import { usePathname } from 'next/navigation';
 import { Toaster } from "react-hot-toast";
@@ -17,8 +18,13 @@ export function Providers({ children }) {
       <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
         <Toaster position="top-right" reverseOrder={false} />
         <div className="flex h-screen bg-gray-50 dark:bg-gray-900">
-          {showSidebar && <Sidebar />}
-          <main className={`flex-1 overflow-auto ${showSidebar ? 'p-6' : ''}`}>
+          {showSidebar && (
+            <>
+              <Sidebar />
+              <Topbar />
+            </>
+          )}
+          <main className={`flex-1 overflow-auto ${showSidebar ? 'pt-16 md:pt-0 md:pl-64 md:p-6' : ''}`}>
             {children}
           </main>
         </div>
