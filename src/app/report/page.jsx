3758be23eb63024d5a5fb83a502c418004ddf5a9
Prince Kwame Baah <134564708+kwamebaah1@users.noreply.github.com>
2026-01-25 12:09:23 +0000

    Reporting Page

---
+++ src/app/report/page.jsx
@@ -0,0 +1,8 @@
+import { PDFReportGenerator } from "@/components/analytics/PDFReportGenerator";
+import React from "react";
+
+export default function ReportPage() {
+  return (
+    <PDFReportGenerator />
+  );
+}
